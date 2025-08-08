a657064121ddf9caf016465d9c191f3981b98cea
joel-molero <joel.molero@gmail.com>
2025-08-08 19:03:43 +0200

    Added goroutines

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
+	"sync"
 )
 
-func main() {
-	timeout := 2 * time.Second
+func scan(port int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	//timeout := 2 * time.Second
 	host := "scanme.nmap.org"
-	for port := 20; port <= 1024; port++ {
-		addr := fmt.Sprintf("%s:%d", host, port)
-		conn, err := net.DialTimeout("tcp", addr, timeout)
 
-		if err != nil {
-			fmt.Printf("port %d closed (or filtered): %v\n", port, err)
-			continue
-		}
-		conn.Close()
-		fmt.Printf("port %d open\n", port)
-		//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-		//status, err := bufio.NewReader(conn).ReadString('\n')
+	addr := fmt.Sprintf("%s:%d", host, port)
+	// conn, err := net.DialTimeout("tcp", addr, timeout)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Printf("port %d closed (or filtered): %v\n", port, err)
+		return
 	}
-	//net.Dial("tcp", "scanme.nmap.org:")
+	conn.Close()
+	fmt.Printf("port %d open\n", port)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for port := 20; port <= 1024; port++ {
+		wg.Add(1)
+		go scan(port, &wg)
+	}
+	wg.Wait()
 }
